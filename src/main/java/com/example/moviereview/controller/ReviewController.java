e4444cffe1d5481a3bfe7d872603f449a904a270
nipun67 <Nitecrawler007@gmail.com>
2019-11-29 12:48:17 +0530

    Added version 2

--- src/main/java/com/example/moviereview/controller/ReviewController.java
+++ src/main/java/com/example/moviereview/controller/ReviewController.java
@@ -1,6 +1,7 @@
 package com.example.moviereview.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -11,4 +12,8 @@ public class ReviewController {
         return "Hello Message";
     }
 
+    @GetMapping(value = "/message/{name}")
+    public String anotherMessage(@PathVariable("name") String name){
+        return "Hello Message" + name;
+    }
 }
